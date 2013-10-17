f084224b1fa1899fe001af1247f54ca52a7c6a59
Angelo Augusto Frozza <frozza@ifc-camboriu.edu.br>
2013-10-17 10:51:41 -0300

    first

---
+++ js/geolocalizacao.js
@@ -0,0 +1,48 @@
+﻿/* Classe Geolocalizacao()
+   Objetive: Possui as classes e métodos para usar Geolocalização
+*/
+function Geolocalizacao() {
+
+   var coords;
+   var timestamp;
+   
+   // Métodos set 
+   this.setCoords = function (coords) {
+      this.coords = coords;
+   }
+   this.setTimestamp = function (timestamp) {
+      this.timestamp = timestamp;
+   }
+
+   // Métodos get
+   this.getCoords = function () {
+      return this.coords;
+   }
+   this.getTimestamp = function () {
+      return this.Timestamp;
+   }
+
+   // Métodos do usuário
+   this.getPosicaoAtual = function() {
+      geolocation.getCurrentPosition(geolocationSuccess, geolocationError, [true]);
+   }
+   
+   function geolocationSucess() {
+      this.setCoords(position.coords);
+      this.setTimestamp(position.timestamp);
+      alert(position.timestamp);
+   }
+   
+   function geolocationError() {
+      return (error.message);
+   }
+   
+   this.toString = function () {
+      return   "Dispositivo: " + this.getModel() +
+             "\nS.O.: " + this.getPlatform() +
+             "\nVersão: " + this.getVersion() +
+             "\nUUID: " + this.getUuid() +
+             "\n** Nome modelo: " + this.getName() +
+             "\nCordova: " + this.getCordova();
+   }
+}
